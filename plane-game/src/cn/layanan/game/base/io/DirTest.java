89dbbfb0af04661be87ac8ec519f24919730d930
Lay <1778038380@qq.com>
2019-07-28 20:52:46 +0800

    文件IO操作

--- plane-game/src/cn/layanan/game/base/io/DirTest.java
+++ plane-game/src/cn/layanan/game/base/io/DirTest.java
@@ -1,6 +1,11 @@
 package cn.layanan.game.base.io;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+import static java.nio.file.FileVisitResult.CONTINUE;
 
 /**
  * 目录操作类
@@ -10,11 +15,12 @@ import java.io.File;
  */
 public class DirTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String dirString = "D:/IDEA Project/Lay Project/base-learning/tmp/user/java";
         String baseDir = "D:/IDEA Project/Lay Project/base-learning/tmp";
         createDir(dirString);
         dirList(baseDir);
+        fileTree(baseDir);
     }
 
 
@@ -45,5 +51,29 @@ public class DirTest {
         } else {
             System.out.println(d.getName() + " is not a directory");
         }
+    }
+
+    private static void fileTree(String dirString) throws IOException {
+        Path path = Paths.get(dirString);
+        FileVisitor<Path> fileVisitor = getFileVisitor();
+
+        Files.walkFileTree(path, fileVisitor);
+    }
+
+    private static FileVisitor<Path> getFileVisitor() {
+        class DirVisitor<Path> extends SimpleFileVisitor<Path> {
+            @Override
+            public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+                System.out.format("%s [Directory]%n", dir);
+                return CONTINUE;
+            }
+
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                System.out.format("%s [File,  Size: %s  bytes]%n", file, attrs.size());
+                return CONTINUE;
+            }
+        }
+        return new DirVisitor<>();
     }
 }
